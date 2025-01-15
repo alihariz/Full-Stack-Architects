edc3c2f282bfce3cdd00f0c7d5d1e5a53ca4c1b2
ALI HARIZ BIN ANUARI A22EC0037 <bear.fred31@gmail.com>
2025-01-15 12:16:27 +0800

    First commit + dependencies

---
+++ low-carbon-initiatives/src/main/java/com/mbip/low_carbon_initiatives/LowCarbonInitiativesApplication.java
@@ -0,0 +1,13 @@
+package com.mbip.low_carbon_initiatives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowCarbonInitiativesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowCarbonInitiativesApplication.class, args);
+	}
+
+}
---
+++ low-carbon-initiatives/src/test/java/com/mbip/low_carbon_initiatives/LowCarbonInitiativesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbip.low_carbon_initiatives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowCarbonInitiativesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
